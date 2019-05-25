d827ec89aa9689c3c7214161ed6e1eda282ebaa7
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-05-25 15:35:12 +0530

    Added template html files in memeapp

--- memeproject/memeapp/urls.py
+++ memeproject/memeapp/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from memeapp import views
+
+app_name = 'memeapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
+    path('register/', views.registerpage, name='registerpage'),
+    path('login/', views.loginpage, name='loginpage')
 ]
--- memeproject/memeapp/views.py
+++ memeproject/memeapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    return render(request=request, template_name='memeapp/homepage.html',
+           context={})
+
+def loginpage(request):
+    return render(request=request, template_name='memeapp/loginpage.html',
+                  context={})
+
+def registerpage(request):
+    return render(request=request, tempalate_name='memeapp/registerpage.html',
+                  context={})
